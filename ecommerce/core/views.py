7ef64452d21514d83a26a3bf6d1aa0ced7da0428
Utkarsh <utkarshkumark@gmail.com>
2020-10-02 20:36:26 +0530

    Home page

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from .models import Item
 
-def item_list(request):
+def home(request):
     context = {
         'items': Item.objects.all()
     }
-    return render(request, 'home-page.html', context)
+    return render(request, 'home.html', context)
