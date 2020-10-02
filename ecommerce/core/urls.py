3e25840c59825114dfaa3a878cbca1dff8153836
Utkarsh <utkarshkumark@gmail.com>
2020-10-03 03:54:38 +0530

    Add items to cart

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -3,7 +3,7 @@ from .views import (
 	Home,
 	Product,
 	checkout,
-
+	add_to_cart,
 	)
 
 app_name = 'core'
@@ -11,6 +11,7 @@ app_name = 'core'
 urlpatterns = [
 	path('', Home.as_view(), name='home'),
     path('product/<slug>/', Product.as_view(), name='product'),
+    path('add-to-cart/<slug>', add_to_cart, name='cart'),  
     path('checkout/', checkout, name='checkout'),
 
 ]
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
-from .models import Item
+from .models import Item, OrderItem, Order
+from django.utils import timezone
 
 
 class Home(ListView):
@@ -11,6 +12,30 @@ class Home(ListView):
 class Product(DetailView):
 	model = Item
 	template_name = 'product.html'
+
+
+def add_to_cart(request, slug):
+	item = get_object_or_404(Item, slug=slug)
+	order_item, created = OrderItem.objects.get_or_create(
+		item = item,
+		user = request.user,
+		ordered = False
+	)
+	order_queryset = Order.objects.filter(user = request.user, ordered = False)
+	if order_queryset.exists():
+		order = order_queryset[0]
+		if order.items.filter(item__slug = item.slug).exists():
+			order_item.quantity += 1
+			order_item.save()
+		else:
+			order.items.add(order_item)
+	else:
+		ordered_date = timezone.now()
+		order = Order.objects.create(
+			user = request.user,
+			ordered_date=ordered_date
+		)
+	return redirect('core:product', slug = slug)
 
 
 def checkout(request):
