eb0375d13fa47efa3675882844a51d53bf8617c3
Utkarsh <utkarshkumark@gmail.com>
2021-01-12 18:38:09 +0530

    Dynamic images

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -134,6 +134,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # All auth
 
